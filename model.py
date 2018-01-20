6d45b9f78ca3120c7f09ad1a8eb2f4b949d2fa55
visonpon <visonpon@gmail.com>
2018-01-20 14:26:05 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from networks import *
 
 #init training data
 
+
 #init model
 self.netG_A = networks.Generator(opt.input_nc, opt.output_nc,
                                         opt.ngf, opt.norm, not opt.no_dropout,opt.gpu_ids)
@@ -16,7 +17,44 @@ self.netG_B = networks.Generator(opt.input_nc, opt.output_nc,
 
 self.netD_A = networks.Discriminator().cuda()
 self.netD_B = networks.Discriminator().cuda()
+
 # training
+print('Training...')
 
+for epoch in range(0, opt.niter+ 1):
+    epoch_start_time = time.time()
+    epoch_iter = 0
 
+    for i, data in enumerate(dataset):
+        iter_start_time = time.time()
+        total_steps += opt.batchSize
+        epoch_iter += opt.batchSize
+        model.set_input(data)
+        model.optimize_parameters()
+
+        if total_steps % opt.display_freq == 0:
+            save_result = total_steps % opt.update_html_freq == 0
+            visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+
+        if total_steps % opt.print_freq == 0:
+            errors = model.get_current_errors()
+            t = (time.time() - iter_start_time) / opt.batchSize
+            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            if opt.display_id > 0:
+                visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+
+        if total_steps % opt.save_latest_freq == 0:
+            print('saving the latest model (epoch %d, total_steps %d)' %
+                  (epoch, total_steps))
+            model.save('latest')
+
+    if epoch % opt.save_epoch_freq == 0:
+        print('saving the model at the end of epoch %d, iters %d' %
+              (epoch, total_steps))
+        model.save('latest')
+        model.save(epoch)
+
+    print('End of epoch %d / %d \t Time Taken: %d sec' %
+          (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+    model.update_learning_rate()
 #save model
