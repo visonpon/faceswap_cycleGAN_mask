81a62b87b13ce0b1c602558dbb7c004f4da7a3f7
visonpon <visonpon@gmail.com>
2018-01-20 11:50:07 +0800

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,12 @@ from mnetworks import *
 #init training data
 
 #init model
+self.netG_A = networks.Generator(opt.input_nc, opt.output_nc,
+                                        opt.ngf, opt.norm, not opt.no_dropout,opt.gpu_ids)
+self.netG_B = networks.Generator(opt.input_nc, opt.output_nc,
+                                        opt.ngf, opt.norm, not opt.no_dropout,opt.gpu_ids)
 
+self.netD_A = networks.Discriminator
 # training
 
 
