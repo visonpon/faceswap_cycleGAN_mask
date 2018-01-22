eb4dac78e8208bf2c4cd12ac59715e2574bdaee5
visonpon <visonpon@gmail.com>
2018-01-22 18:11:26 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 import option as op
 from networks import *
+import model
 
 #init some hyperparameter
 
@@ -39,17 +40,18 @@ for epoch in range(0, opt.niter+ 1):
         model.set_input(data)
         model.optimize_parameters()
 
-        if total_steps % opt.display_freq == 0:
-            save_result = total_steps % opt.update_html_freq == 0
-            visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-
         if total_steps % opt.print_freq == 0:
-            errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
-            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-            if opt.display_id > 0:
-                visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
-
+            
+            print('Epoch #%d' % (epoch+1))
+            print('Batch #%d' % opt.batchSize)
+            print('Loss G_A: %0.3f' % loss_G_A.data[0] + '\t' +
+                  'Loss G_B: %0.3f' % loss_G_A.data[0])
+            print('Loss D_A: %0.3f' % loss_D_A.data[0] + '\t' +
+                  'Loss D_B: %0.3f' % loss_D_A.data[0])
+            print('Loss D_A: %0.3f' % loss_D_A.data[0] + '\t' +
+                  'Loss D_B: %0.3f' % loss_D_A.data[0])
+            print('-'*50)
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' %
                   (epoch, total_steps))
@@ -60,6 +62,9 @@ for epoch in range(0, opt.niter+ 1):
               (epoch, total_steps))
         model.save('latest')
         model.save(epoch)
+    print('End of epoch %d / %d \t Time Taken: %d sec' %
+          (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+    model.update_learning_rate()
 
     print('End of epoch %d / %d \t Time Taken: %d sec' %
           (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
