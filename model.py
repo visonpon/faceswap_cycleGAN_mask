fcfdb4dbb8ac6e52f1d34923803bd7985fa73b7b
visonpon <visonpon@gmail.com>
2018-01-22 13:56:52 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import networks
 import option as opt
 from PIL import Image
-
+import torch
 def initialize(self, opt):
         self.opt = opt
         self.gpu_ids = opt.gpu_ids
@@ -112,10 +112,10 @@ def backward_D_basic(self, netD, real, fake):
         # Identity loss
         if lambda_idt > 0:
             # G_A should be identity if real_B is fed.
-            idt_A = self.netG_A(self.real_B)
+            idt,_ = self.netG_A(self.real_B)
             loss_idt_A = self.criterionIdt(idt_A, self.real_B) * lambda_B * lambda_idt
             # G_B should be identity if real_A is fed.
-            idt_B = self.netG_B(self.real_A)
+            idt_B,_ = self.netG_B(self.real_A)
             loss_idt_B = self.criterionIdt(idt_B, self.real_A) * lambda_A * lambda_idt
 
             self.idt_A = idt_A.data
@@ -129,21 +129,23 @@ def backward_D_basic(self, netD, real, fake):
             self.loss_idt_B = 0
 
         # GAN loss D_A(G_A(A))
-        fake_B = self.netG_A(self.real_A)
+        fake_B,mask_B = self.netG_A(self.real_A)
         pred_fake = self.netD_A(fake_B)
         loss_G_A = self.criterionGAN(pred_fake, self.target_tensor)#################
+        loss_G_A += torch.mean(torch.abs(mask_B))
 
         # GAN loss D_B(G_B(B))
-        fake_A = self.netG_B(self.real_B)
+        fake_A,mask_A = self.netG_B(self.real_B)
         pred_fake = self.netD_B(fake_A)
         loss_G_B = self.criterionGAN(pred_fake, self.target_tensor)
+        loss_G_B += torch.mean(torch.abs(mask_A))
 
         # Forward cycle loss
-        rec_A = self.netG_B(fake_B)
+        rec_A,_ = self.netG_B(fake_B)
         loss_cycle_A = self.criterionCycle(rec_A, self.real_A) * lambda_A
 
         # Backward cycle loss
-        rec_B = self.netG_A(fake_A)
+        rec_B,_ = self.netG_A(fake_A)
         loss_cycle_B = self.criterionCycle(rec_B, self.real_B) * lambda_B
         # combined loss
         loss_G = loss_G_A + loss_G_B + loss_cycle_A + loss_cycle_B + loss_idt_A + loss_idt_B
