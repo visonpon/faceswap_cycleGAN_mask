01d89d7f89564bc0ab00bb004bd2e4f8d09c2877
visonpon <visonpon@gmail.com>
2018-01-29 15:47:37 +0800

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,9 +1,10 @@
-import torch.utils.data
+import torch.utils.data as data
 import os
 import os.path
 import torchvision.transforms as transforms
 from PIL import Image
 import random
+import option as opt
 from data.base_data_loader import BaseDataLoader
 
 def make_dataset(dir):
@@ -26,21 +27,22 @@ def __scale_width(img, target_width):
     w = target_width
     h = int(target_width * oh / ow)
     return img.resize((w, h), Image.BICUBIC)
-def get_transform():
+
+def get_transform(opt):
     transform_list = []
     if opt.resize_or_crop == 'resize_and_crop':
-        osize = [loadSize, loadSize]
+        osize = [opt.loadSize, opt.loadSize]
         transform_list.append(transforms.Scale(osize, Image.BICUBIC))
-        transform_list.append(transforms.RandomCrop(fineSize))
+        transform_list.append(transforms.RandomCrop(opt.fineSize))
     elif opt.resize_or_crop == 'crop':
-        transform_list.append(transforms.RandomCrop(fineSize))
+        transform_list.append(transforms.RandomCrop(opt.fineSize))
     elif opt.resize_or_crop == 'scale_width':
         transform_list.append(transforms.Lambda(
-            lambda img: __scale_width(img,fineSize)))
+            lambda img: __scale_width(img,opt.fineSize)))
     elif opt.resize_or_crop == 'scale_width_and_crop':
         transform_list.append(transforms.Lambda(
-            lambda img: __scale_width(img, loadSize)))
-        transform_list.append(transforms.RandomCrop(fineSize))
+            lambda img: __scale_width(img, opt.loadSize)))
+        transform_list.append(transforms.RandomCrop(opt.fineSize))
 
     if isTrain and not no_flip:
         transform_list.append(transforms.RandomHorizontalFlip())
@@ -50,12 +52,32 @@ def get_transform():
                                             (0.5, 0.5, 0.5))]
     return transforms.Compose(transform_list)
 
+class BaseDataLoader():
+    def __init__(self):
+        pass
     
-class UnalignedDataset():
-    def initialize(self):
-        self.root = dataroot
-        self.dir_A = os.path.join(dataroot, phase + 'A')
-        self.dir_B = os.path.join(dataroot, phase + 'B')
+    def initialize(self, opt):
+        self.opt = opt
+        pass
+
+    def load_data():
+        return None
+    
+class BaseDataset(data.Dataset):
+    def __init__(self):
+        super(BaseDataset, self).__init__()
+
+    def name(self):
+        return 'BaseDataset'
+
+    def initialize(self, opt):
+        pass
+    
+class UnalignedDataset(BaseDataset):
+    def initialize(self,opt):
+        self.root = opt.dataroot
+        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')
+        self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')
 
         self.A_paths = make_dataset(self.dir_A)
         self.B_paths = make_dataset(self.dir_B)
@@ -103,26 +125,27 @@ class UnalignedDataset():
     def name(self):
         return 'UnalignedDataset'
         
-class CustomDatasetDataLoader():
+class CustomDatasetDataLoader(BaseDataLoader):
     def name(self):
         return 'CustomDatasetDataLoader'
 
-    def initialize(self):
-        self.dataset = UnalignedDataset()
-        self.dataloader = torch.utils.data.DataLoader(
+    def initialize(self,opt):
+        BaseDataLoader.initialize(self, opt)
+        self.dataset = UnalignedDataset(opt)
+        self.dataloader = data.DataLoader(
             self.dataset,
-            batch_size=batchSize,
-            shuffle=not serial_batches,
-            num_workers=int(nThreads))
+            batch_size=opt.batchSize,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.nThreads))
 
     def load_data(self):
         return self
 
     def __len__(self):
-        return min(len(self.dataset), self.max_dataset_size)
+        return min(len(self.opt.dataset), self.opt.max_dataset_size)
 
     def __iter__(self):
-        for i, data in enumerate(self.dataloader):
-            if i >= self.max_dataset_size:
+        for i, data in enumerate(self.opt.dataloader):
+            if i >= self.opt.max_dataset_size:
                 break
             yield data
