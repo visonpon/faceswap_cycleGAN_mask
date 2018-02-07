2ef9bffb64378cd8031704c17300a814127310b9
visonpon <visonpon@gmail.com>
2018-02-07 18:04:56 +0800

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,3 +1,55 @@
+import dominate
+from dominate.tags import *
+import os
+import option as opt
+import util
+
+class HTML:
+    def __init__(self, web_dir, title, reflesh=0):
+        self.title = title
+        self.web_dir = web_dir
+        self.img_dir = os.path.join(self.web_dir, 'images')
+        if not os.path.exists(self.web_dir):
+            os.makedirs(self.web_dir)
+        if not os.path.exists(self.img_dir):
+            os.makedirs(self.img_dir)
+        # print(self.img_dir)
+
+        self.doc = dominate.document(title=title)
+        if reflesh > 0:
+            with self.doc.head:
+                meta(http_equiv="reflesh", content=str(reflesh))
+
+    def get_image_dir(self):
+        return self.img_dir
+
+    def add_header(self, str):
+        with self.doc:
+            h3(str)
+
+    def add_table(self, border=1):
+        self.t = table(border=border, style="table-layout: fixed;")
+        self.doc.add(self.t)
+
+    def add_images(self, ims, txts, links, width=400):
+        self.add_table()
+        with self.t:
+            with tr():
+                for im, txt, link in zip(ims, txts, links):
+                    with td(style="word-wrap: break-word;", halign="center", valign="top"):
+                        with p():
+                            with a(href=os.path.join('images', link)):
+                                img(style="width:%dpx" % width, src=os.path.join('images', im))
+                            br()
+                            p(txt)
+
+    def save(self):
+        html_file = '%s/index.html' % self.web_dir
+        f = open(html_file, 'wt')
+        f.write(self.doc.render())
+        f.close()
+    
+    
 class Visualizer():
     def __init__(self, opt):
         # self.opt = opt
@@ -71,7 +123,7 @@ class Visualizer():
                     img_path = os.path.join(self.img_dir, 'epoch%.3d_%s.png' % (epoch, label))
                     util.save_image(image_numpy, img_path)
                 # update website
-                webpage = html.HTML(self.web_dir, 'Experiment name = %s' % self.name, reflesh=1)
+                webpage = HTML(self.web_dir, 'Experiment name = %s' % self.name, reflesh=1)
                 for n in range(epoch, 0, -1):
                     webpage.add_header('epoch [%d]' % n)
                     ims = []
