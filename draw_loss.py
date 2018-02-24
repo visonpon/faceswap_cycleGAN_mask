4620d2a971aea339b85ff49850e5d30dd7315c72
visonpon <visonpon@gmail.com>
2018-02-24 14:09:14 +0800

    Update draw_loss.py

--- draw_loss.py
+++ draw_loss.py
@@ -11,24 +11,6 @@ import re
 import pylab  
 from pylab import figure, show, legend  
 from mpl_toolkits.axes_grid1 import host_subplot  
-vis = visdom.Visdom(port=[8097])
-
-def plot_current_errors(self, epoch, counter_ratio, opt, errors):
-        self.name= 'experiment_name'
-        self.display_id = 1
-        if not hasattr(self,'plot_data'):
-            self.plot_data = {'X': [], 'Y': [], 'legend': list(errors.keys())}
-        self.plot_data['X'].append(epoch + counter_ratio)
-        self.plot_data['Y'].append([errors[k] for k in self.plot_data['legend']])
-        vis.line(
-            X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
-            Y=np.array(self.plot_data['Y']),
-            opts={
-                'title': self.name + ' loss over time',
-                'legend': self.plot_data['legend'],
-                'xlabel': 'epoch',
-                'ylabel': 'loss'},
-            win=self.display_id)
         
 ###############################################################
 #read from log file and then draw the loss curve.
